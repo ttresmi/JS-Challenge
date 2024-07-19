ecd3c0bcb718542342deabdc7cde68180f8ea5c1
ttresmi <98709101+ttresmi@users.noreply.github.com>
2024-07-19 04:49:20 +0000

    day4 commit

---
+++ Day04/loop.js
@@ -0,0 +1,17 @@
+for(let i=1;i<=10;i++){
+   
+    if(i==5){
+        continue;
+    }
+    console.log("Numbers",i)     
+}
+console.log("*********************Task 9T********************************")
+
+for(let i=1;i<=10;i++){
+   
+    if(i==7){
+       break
+    }
+    console.log("Numbers",i)     
+}
+console.log("End")
