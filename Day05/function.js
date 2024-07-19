0094da5fc65c60e8056adad2392b417fbd61d5d8
ttresmi <98709101+ttresmi@users.noreply.github.com>
2024-07-19 07:05:07 +0000

    functions

---
+++ Day05/function.js
@@ -0,0 +1,22 @@
+console.log("***********************Task 1***************************")
+
+function checkEvenorOdd(number){
+    if(number%2==0)
+        console.log("The entered number is even")
+    else
+        console.log(`${number} is a odd number`)
+
+}
+
+checkEvenorOdd(589)
+
+console.log("***********************Task 2***************************")
+function findSquar(number){
+    return number*number
+
+}
+
+let squarofnumber=findSquar(5)
+console.log("Square --",squarofnumber)
+
+console.log("***********************Task 3***************************")
