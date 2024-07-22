1c124436e12c6c1c774c34f18c8da8c4cf9c7c67
ttresmi <98709101+ttresmi@users.noreply.github.com>
2024-07-22 10:13:59 +0000

    Day08

--- Day07/object.js
+++ Day07/object.js
@@ -3,3 +3,14 @@ let book={
     author:"K Sharma",
     year:1998
 }
+
+for(let key in book){
+    console.log(`Object is ${key} :${book[key]}`)
+}
+
+let objectKey=Object.keys(book)
+console.log("Properties.",objectKey)
+objectKey.forEach((key)=>console.log("values are..",book[key]))
+
+let objectValues=Object.values(book)
+console.log("values..",objectValues)
